00ee2112ccee9b621cda09bdd493242e3f005d79
greenprofessionals <green.professionals.network@gmail.com>
2026-08-08 23:04:47 -0400

    Update app-config

--- assets/js/app-config.js
+++ assets/js/app-config.js
@@ -1,6 +1,6 @@
 /* Public web configuration. Apps Script /exec URLs are not passwords. */
 window.SLPP_APP_CONFIG = Object.freeze({
-  workflowEndpoint: 'https://script.google.com/macros/s/AKfycby_mn8mUePqH5BXRDRhwNk0Ffs66XtzVZDybvkfGtEqx5oBnAG-D9FczTCDCM6-vIVu/exec',
+  workflowEndpoint: 'https://script.google.com/macros/s/AKfycbws9MVioq1zBgLlDDxKuy-K7H2raZG2Dt1nx7TlNVb6tcBW6TkA-_N6FokwVJulE7hK/exec',
   ticketConfigEndpoint: 'https://script.google.com/macros/s/AKfycbyZL6FHkp2XBmVtqa3LSHWEy0hj2HCSSdLaUKUYeKumoPb5VaoVn0BjmwZFzLHfWblNLA/exec',
   defaultOrganization: 'SLPPNA',
   defaultChapter: 'NYC',
