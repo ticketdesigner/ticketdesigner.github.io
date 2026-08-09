c15e19089c00f4ca6a027f0076d9b1e567b1bec6
greenprofessionals <green.professionals.network@gmail.com>
2026-08-09 09:26:33 -0400

    updated app-config

--- assets/js/app-config.js
+++ assets/js/app-config.js
@@ -1,10 +1,9 @@
 /* Ticket Designer v7 public web configuration.
  * Paste the NEW Apps Script /exec URLs after deploying the two fresh backends.
  */
+
 window.SLPP_APP_CONFIG = Object.freeze({
-  workflowEndpoint: 'PASTE_NEW_WORKFLOW_EXEC_URL',
-  ticketConfigEndpoint: 'PASTE_NEW_REGIONAL_CONFIG_EXEC_URL',
-  defaultOrganization: 'SLPPNA',
-  defaultChapter: '',
-  defaultEvent: ''
+  workflowEndpoint: 'https://script.google.com/macros/s/AKfycbyYjKF-9Ew8ZeYY00ij2eFTuptFEQvlmi6Sfpgg2RXFilmbZLoCMSuTru8vFySQFsAN4w/exec',
+  ticketConfigEndpoint: 'https://script.google.com/macros/s/AKfycbxzNuRRbOEj8dUp-IT2_RCdMmVouUu30PAKuMXYSUk2xPFtK1wqpZI3mRtdlb0IWIJ2ow/exec',
+  defaultOrganization: 'SLPPNA'
 });
