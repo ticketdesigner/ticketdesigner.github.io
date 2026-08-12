0c1f97fd6a4705950bc4efdd4060fd5c231bfced
ticketdesigner <ticket.designer.sl@gmail.com>
2026-08-12 05:51:46 -0400

    Update ticketConfigEndpoint URL in app-config.js

--- assets/js/app-config.js
+++ assets/js/app-config.js
@@ -4,6 +4,6 @@
 
 window.SLPP_APP_CONFIG = Object.freeze({
   workflowEndpoint: 'https://script.google.com/macros/s/AKfycbyYjKF-9Ew8ZeYY00ij2eFTuptFEQvlmi6Sfpgg2RXFilmbZLoCMSuTru8vFySQFsAN4w/exec',
-  ticketConfigEndpoint: 'https://script.google.com/macros/s/AKfycbyYjKF-9Ew8ZeYY00ij2eFTuptFEQvlmi6Sfpgg2RXFilmbZLoCMSuTru8vFySQFsAN4w/exec',
+  ticketConfigEndpoint: 'https://script.google.com/macros/s/AKfycbzb3JKqTAksqsNMsIPv8ewIOPjBiR8J5vZXEe7e6JqEBd6h48Iea5hKC8jlRkKDdN-Zog/exec',
   defaultOrganization: 'SLPPNA'
 });
